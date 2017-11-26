4d24e68adff2782b693d702b7d23353bef85d78b
XITENG-PC\LiuXiteng <LiuXiteng@sjtu.edu.cn>
2017-11-26 19:09:01 +0800

    modify example on 172.2

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 import graph, BFS
 
 def graph_1():
-    g=graph.graph();
-    # g=NewDense(6);
+    g=graph.graph()
 
-    g.AddVertex('a');
-    g.AddVertex('b');
-    g.AddVertex('c');
-    g.AddVertex('d');
-    g.AddVertex('e');
-    g.AddVertex('t');
+    g.AddVertex('s')
+    g.AddVertex('b')
+    g.AddVertex('c')
+    g.AddVertex('d')
+    g.AddVertex('e')
+    g.AddVertex('t')
 
-    g.AddEdge('a','b',-4);
-    g.AddEdge('a','t',-3);
-    g.AddEdge('b','d',-1);
-    g.AddEdge('b','e',-2);
-    g.AddEdge('c','b',8);
-    g.AddEdge('c','t',3);
-    g.AddEdge('d','a',6);
-    g.AddEdge('d','t',4);
-    g.AddEdge('e','c',-3);
-    g.AddEdge('e','t',2);
+    g.AddEdge('s','b',10)
+    g.AddEdge('s','d',10)
+
+    g.AddEdge('b','c',9)
+
+    g.AddEdge('c','e',6)
+    g.AddEdge('c','t',10)
+        
+    g.AddEdge('d','b',2)
+    g.AddEdge('d','c',8)
+    g.AddEdge('d','e',4)
+
+    g.AddEdge('e','t',10)
 
     return g
 

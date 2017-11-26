b1584cac2f164ab81d66760eb06e4ac4ba5777cb
XITENG-PC\LiuXiteng <LiuXiteng@sjtu.edu.cn>
2017-11-26 17:45:38 +0800

    implement Breadth-First Search(BFS)

--- BFS.py
+++ BFS.py
@@ -1,3 +1,18 @@
 import graph
-def BFS(g):
-    pass
+def BFS(G, s, INF=99999):
+    dists = dict()
+    prevs = dict()
+    for v in G.g:
+        dists[v] = INF
+        prevs[v] = None
+    Q = []
+    dists[s] = 0
+    Q.append(s)
+    while len(Q)!=0:
+        v = Q.pop(0)
+        for u in G.g[v]:
+            if dists[u]==INF:
+                dists[u] = dists[v]+1
+                prevs[u] = v
+                Q.append[u]
+                print('%s is visited'%u)

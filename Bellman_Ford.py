041ebbc3638f6f9d1b5a8eb1690bdaad909ad282
xiteng <LiuXiteng@sjtu.edu.cn>
2017-11-27 23:02:24 +0800

    Use in-class parameters dists&prevs

--- Bellman_Ford.py
+++ Bellman_Ford.py
@@ -1,23 +1,21 @@
 import graph
 def BellmanFord(G,s,t,INF=99999):
-    dists = dict()
-    prevs = dict()
     for v in G.g:
         if v!=s:
-            dists[v] = INF
-            prevs[v] = None
+            G.dists[v] = INF
+            G.prevs[v] = None
         else:
-            dists[v] = 0
+            G.dists[v] = 0
     for count in range(1,len(G.g)):
         for u in G.g:
             for v in G.g[u]:
-                tmp = dists[u] + G.g[u][v]
-                if tmp<dists[v]:
-                    dists[v] = tmp
-                    prevs[v] = u
+                tmp = G.dists[u] + G.g[u][v]
+                if tmp<G.dists[v]:
+                    G.dists[v] = tmp
+                    G.prevs[v] = u
     path = []
     while True:
         path.append(t)
-        t = prevs[t]
+        t = G.prevs[t]
         if t==s:
             return path+[s]
--- Dijkstra.py
+++ Dijkstra.py
@@ -1,33 +1,31 @@
 import graph, fib_heap
 
 def Dijkstra(G,s,t,INF=99999):
-    dists = dict()
-    prevs = dict()
     nodes = dict()
     S=fib_heap.FibonacciHeap()
     for v in G.g:
         if v==s:
-            dists[v]=0
+            G.dists[v]=0
         else:
-            dists[v]=INF
-        prevs[v]=None
-        node_v=fib_heap.FibonacciHeap.Node(dists[v],v)
+            G.dists[v]=INF
+        G.prevs[v]=None
+        node_v=fib_heap.FibonacciHeap.Node(G.dists[v],v)
         nodes[v]=node_v
         S.Insert(node_v)
     v = S.extract_min().value
     while True:
         for u in G.g[v]:
-            tmp = dists[v]+G.g[v][u]
-            if tmp<dists[u]:
-                dists[u]=tmp
+            tmp = G.dists[v]+G.g[v][u]
+            if tmp<G.dists[u]:
+                G.dists[u]=tmp
                 S.decrease_key(nodes[u],tmp)
-                prevs[u]=v
+                G.prevs[u]=v
         v=S.extract_min().value
         if v==t:
             break
     path = []
     while True:
         path.append(t)
-        t = prevs[t]
+        t = G.prevs[t]
         if t==s:
             return path+[s]
